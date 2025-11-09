f801a92b85896cec6241c59ae3419979de81ad01
daffarmd <muhammaddaffarmd@gmail.com>
2025-11-09 17:42:34 +0700

    feat: template

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,5 @@
+<script>
+    const src = '/logo-removebg-preview.png';
+</script>
+
+<img src={src} alt="Logo" />
---
+++ src/routes/hello/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Logo from "$lib/components/Logo.svelte";
+
+    const name = 'Hello World';
+    const fullname = "John Doe";
+
+    const src = '/logo-removebg-preview.png';
+</script>
+
+<style>
+    h1 {
+        size: 100px;
+        color: rgb(255, 0, 0);
+        font-family: Papyrus, sans-serif;
+    }
+</style>
+
+<h1>{name}</h1>
+
+<p>Nama Lengkap {fullname.toUpperCase()}</p>
+
+<Logo/>
