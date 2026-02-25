170907a692e6bfee7edf5f392799f42ce82ff92e
daffarmd <muhammaddaffarmd@gmail.com>
2026-02-25 10:31:14 +0700

    feat: template(Text Expresion, Dynamic Attribute, HTML Tag)

--- src/lib/Hello.svelte
+++ src/lib/Hello.svelte
@@ -1,11 +1,40 @@
 <script>
+  import Logo from "./Logo.svelte";
+
+    // Hello
     alert('Hello, World!');
+
+    // Text Expresion
+    const name = 'Text Expresion';
+
+    // Dynamic Attribute
+    const src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/3840px-Bitcoin.svg.png';
+
+    // HTML Tag
+    const content = '<h1> Hello </h1>';
 </script>
 
 <style>
     h1 {
         color: blue;
     }
+
+    img {
+        width: 200px;
+    }
 </style>
 
+<!-- Hello -->
 <h1> Hello </h1>
+
+<!-- Text Expresion -->
+<h1> Hello {name.toLowerCase()} </h1>
+
+<!-- Dynamic Attribute -->
+<img {src} alt="www">
+
+<!-- Nested Component -->
+<Logo />
+
+<!-- HTML Tag -->
+{@html content}
---
+++ src/lib/Logo.svelte
@@ -0,0 +1,11 @@
+<script>
+    const src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/3840px-Bitcoin.svg.png';
+</script>
+
+<style>
+    img {
+        width: 200px;
+    }
+</style>
+
+<img {src} alt="www">
